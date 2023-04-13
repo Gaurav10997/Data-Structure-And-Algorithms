d172e3ee6818073bbff91dc3649d30d24e9a3fc5
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-04-14 04:59:29 +0530

    Time: 5 ms (51.29%), Space: 43.6 MB (17.73%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,65 +1,74 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        boolean[][] matrix = new boolean[n][n];
+        boolean [][] matrix = new boolean [n][n];
         return NQueens(matrix,0);
+        
     }
-    
-    static List<List<String>> NQueens(boolean[][] matrix, int r) {
-    List<List<String>> ans = new ArrayList<>();
-    if (r == matrix.length) {
-        ans.add(queens(matrix,r));
-        return ans;
-    }
-    for (int c = 0; c < matrix.length; c++) {
-        if (isSafe(matrix, r, c)) {
-            matrix[r][c] = true;
-            ans.addAll(NQueens(matrix, r + 1));
-            //adding all answers together from different function calls because new list created everytime.
-            matrix[r][c] = false;
-            //backtracking
+    // main helper function hai 
+    static List<List<String>> NQueens(boolean[][] matrix , int row){
+        
+        List<List<String>> ans = new ArrayList<>();
+        
+        if(row==matrix.length){
+            ans.add(Queens(matrix));
+            return ans;
         }
-    }
-    return ans;
-}
- static boolean isSafe(boolean[][] matrix, int r, int c) {
-    //Vertical up
-    for (int i = 0; i <r; i++) {
-        if (matrix[i][c]) {
-            return false;
-        }
-    }
-    //Diagonal left
-	
-    int minLeft = Math.min(r,c);
-    for (int i = 0; i <= minLeft; i++) {
-        if (matrix[r - i][c - i]) {
-            return false;
-        }
-    }
-    //Diagonal right
-	
-    int maxRight = Math.min(r, matrix.length-c-1)  ;
-    for (int i = 0; i <=maxRight; i++) {
-        if (matrix[r - i][c + i]) {
-            return false;
-        }
-    }
-    return true;
-}
-static List<String> queens(boolean[][] matrix, int r) {
-    List<String> ans = new ArrayList<>();
-    //This generate Arraylist in form of Q and . by comparing with boolean matrix
-    for (boolean[] row : matrix) {
-        String str="" ;
-        for (boolean element : row) {
-            if (element == true) {
-                str+="Q";
-            } else {
-                str+=".";
+        
+        for(int c = 0 ; c<matrix.length ; c++){
+            if(canPlace(matrix, row , c)){
+                matrix[row][c]=true;
+                ans.addAll(NQueens(matrix, row + 1));
+                matrix[row][c]=false;
             }
         }
-        ans.add(str);
+        return ans;
     }
-    return ans;
-}
+    
+    static boolean canPlace(boolean matrix[][],int row , int col){
+        for(int i = 0 ; i< row ; i++){
+            if(matrix[i][col]){
+                return false;
+            }
+            
+        }
+        int minleft = Math.min(row,col);
+        for(int i = 0 ; i<=minleft ; i++ ){
+            if(matrix[row-i][col-i]){
+                return false;
+            }
+        }
+        
+        int minright = Math.min(row, matrix.length-1-col);
+        for(int i = 0 ; i<=minright ; i++){
+            if(matrix[row-i][col+i]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    
+    
+    
+    
+    
+    static List<String> Queens(boolean[][] matrix){
+        List<String> ans = new ArrayList<>();
+        for(boolean [] row : matrix){
+            String str="" ;
+            for(boolean el : row){
+                if(el){
+                    str+="Q";
+                }else{
+                    str+=".";
+                }
+            }
+            ans.add(str);
+            
+        }
+        return ans;
+    }
+
 }
