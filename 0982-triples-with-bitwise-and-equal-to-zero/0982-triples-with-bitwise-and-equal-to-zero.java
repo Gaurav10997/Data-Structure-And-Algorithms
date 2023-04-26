ba4d3d6bae2e856ba92192796055e8ee8eb5b84e
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-04-26 11:12:08 +0530

    Time: 764 ms (23.30%), Space: 39.9 MB (100.00%) - LeetHub

---
+++ 0982-triples-with-bitwise-and-equal-to-zero/0982-triples-with-bitwise-and-equal-to-zero.java
@@ -0,0 +1,26 @@
+class Solution {
+    //tc : n cubed
+    public int countTriplets(int[] A) {
+        int count = 0;
+        Arrays.sort(A);
+        for (int i = 0 ; i < A.length; i++) {
+            for (int j = i; j < A.length; j++) {
+                int tmp = A[i] & A[j];
+                for (int k = j; k < A.length; k++) {
+                    int re = tmp & A[k];
+                    if (re == 0) {
+                        if (i == j && j == k) {
+                            count += 1;
+                        } else if (i == j || i == k || j == k) {
+                            count += 3;
+                        } else {
+                            count += 6;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+}
