ee294944b1ad6740b5f3c8b968e4e2ef2ec23c22
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-04-26 06:30:44 +0530

    Time: 592 ms (5.40%), Space: 43.2 MB (18.92%) - LeetHub

--- 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.java
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.java
@@ -1,38 +1,47 @@
 class Solution {
-   public int maxSumSubmatrix(int[][] matrix, int k) {
-   
-    int rows = matrix.length, cols = matrix[0].length;
-    int[][] areas = new int[rows][cols];
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            int area = matrix[r][c];
-            if (r-1 >= 0)
-                area += areas[r-1][c];
-            if (c-1 >= 0)
-                area += areas[r][c-1];
-            if (r-1 >= 0 && c-1 >= 0)
-                area -= areas[r-1][c-1];
-            areas[r][c] = area;
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        // prefix sum of this matrix ;
+        int rows = matrix.length , cols = matrix[0].length;
+        int prefixSum[][] = new int[rows][cols];
+        
+        for(int row = 0 ; row<rows ; row++){
+            for(int col = 0 ; col<cols ; col++){
+                int area = matrix[row][col];
+                if(row-1>=0){
+                    area+=prefixSum[row-1][col];
+                }
+                if(col-1>=0){
+                    area+=prefixSum[row][col-1];
+                }
+                if(row-1>=0 && col-1>=0){
+                     area-=prefixSum[row-1][col-1];
+                }
+                prefixSum[row][col]=area;               
+                
+            }
         }
-    }
-    int max = Integer.MIN_VALUE;
-    for (int r1 = 0; r1 < rows; r1++) {
-        for (int c1 = 0; c1 < cols; c1++) {
-            for (int r2 = r1; r2 < rows; r2++) {
-                for (int c2 = c1; c2 < cols; c2++) {
-                    int area = areas[r2][c2];
-                    if (r1-1 >= 0)
-                        area -= areas[r1-1][c2];
-                    if (c1-1 >= 0)
-                        area -= areas[r2][c1-1];
-                    if (r1-1 >= 0 && c1 -1 >= 0)
-                        area += areas[r1-1][c1-1];
-                    if (area <= k)
-                        max = Math.max(max, area);
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0 ; i< rows ; i++){
+            for(int j = 0 ; j< cols; j++){
+                for(int m = i ; m<rows ; m++){
+                    for(int n = j; n<cols ; n++){
+                        int area = prefixSum[m][n];
+                        if(i-1>=0){
+                            area-=prefixSum[i-1][n];                                                             
+                        }
+                         if(j-1>=0){
+                            area-=prefixSum[m][j-1];
+                        }
+                         if(j-1>=0 && i-1>=0){
+                            area+=prefixSum[i-1][j-1];
+                        }
+                        if(area<=k){
+                            ans = Math.max(ans,area);
+                        }
+                    }
                 }
             }
         }
+        return ans;
     }
-    return max;
-}
 }
