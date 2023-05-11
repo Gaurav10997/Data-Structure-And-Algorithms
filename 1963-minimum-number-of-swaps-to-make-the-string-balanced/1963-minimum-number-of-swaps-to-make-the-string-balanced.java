73e7f5870187ab04bd27b8cd0f131a4fb1e2af63
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-05-11 15:54:34 +0530

    Time: 19 ms (93.76%), Space: 50.9 MB (52.83%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,18 +1,30 @@
 class Solution {
-    public int minSwaps(String s) {
-        Stack<Character> st = new Stack<>();
-        int mismatch = 0;
-        for(int i = 0 ; i< s.length() ; i++){
-            if(s.charAt(i) == '[' ){
-                st.push(']');
-            }else{
-                if(!st.isEmpty()){
-                    st.pop();
-                }else{
-                    mismatch++;
-                }
-            }
-        }
-        return (mismatch+1)/2;
+    public int minSwaps(String brackets) {
+        // Stack<Character> st = new Stack<>();
+        // int mismatch = 0;
+        // for(int i = 0 ; i< s.length() ; i++){
+        //     if(s.charAt(i) == '[' ){
+        //         st.push(']');
+        //     }else{
+        //         if(!st.isEmpty()){
+        //             st.pop();
+        //         }else{
+        //             mismatch++;
+        //         }
+        //     }
+        // }
+        // return (mismatch+1)/2;
+        int size = 0 ; 
+
+		for(int i = 0 ; i< brackets.length() ; i++){
+			if(brackets.charAt(i)=='['){
+				size++;
+			}else{
+				if(size>0){
+					size--;
+				}
+			}
+		}
+		return (size+1)/2;
     }
 }
