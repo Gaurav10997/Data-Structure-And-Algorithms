922be31e08ae2c752d4653f4aba982cc0d90a36c
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-08-03 02:59:34 +0530

    Time: 6 ms (11.76%), Space: 41.5 MB (35.68%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        String [] arr = {"" , "", "abc" ,"def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String> list = new ArrayList<>();
+        if(digits.equals(""))return list;
+        recursion(arr,list,digits,"");
+        return list;
+    }
+    public void  recursion(String [] arr,List<String> list , String up,String p){
+        if(up.equals("")){
+            list.add(p);
+            return;
+        }
+        int firstchar = up.charAt(0)-'0';
+        int len = arr[firstchar].length();
+        up= up.substring(1);
+        for(int i = 0 ; i<len ; i++){
+            recursion(arr,list,up,p+arr[firstchar].charAt(i));
+        }
+    }
+}
