853e38c58596c47f766bb9dfafa3f39e509b241d
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-08-24 11:55:27 +0530

    Time: 4 ms (27.63%), Space: 40.8 MB (51.32%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String makeLargestSpecial(String S) {
+
+            int balance = 0, l = 0;
+            List<String> subResults = new LinkedList<>();
+            for (int r = 0; r < S.length(); r++) {
+                if (S.charAt(r) == '0') {
+                    balance--;
+                }
+                else {
+                    balance++;
+                }
+                if (balance == 0) {
+                    subResults.add("1" + makeLargestSpecial(S.substring(l + 1, r)) + "0");
+                    l = r + 1;
+                }
+            }
+            Collections.sort(subResults, Collections.reverseOrder());
+
+            return String.join("", subResults);
+        }
+}
