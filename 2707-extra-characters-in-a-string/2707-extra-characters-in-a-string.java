51b2259176309205af10827a11eac25d45e5f146
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-09-02 16:13:01 +0530

    Time: 42 ms (73.43%), Space: 44.2 MB (56.00%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -6,14 +6,15 @@ class Solution {
             set.add(key);
         }
         HashMap<Integer,Integer> dp = new HashMap<>();
-        
+        dp.put(s.length(),0);
         return dfs(0,s,set,dp);
     }
     
     public int dfs (int i,String s, HashSet<String> words, HashMap<Integer,Integer> dp ){
-        if(i==s.length()) return 0;
+   
         if(dp.containsKey(i))return dp.get(i);
         res=1+dfs(i+1,s,words,dp);
+        
         for(int j = i ; j<s.length() ; j++){
             if(words.contains(s.substring(i,j+1))){
                 res = Math.min(dfs(j+1,s,words,dp),res);
