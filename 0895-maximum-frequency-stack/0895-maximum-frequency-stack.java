8aa284c6b520adcb88f84902071aedc310ef9857
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-09-04 21:19:50 +0530

    Time: 29 ms (77.83%), Space: 55.1 MB (65.63%) - LeetHub

---
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.java
@@ -0,0 +1,39 @@
+class FreqStack {
+
+    List<Stack<Integer>> list ;
+    HashMap<Integer,Integer> map ;
+    public FreqStack() {
+        list = new ArrayList<>();
+        map=new HashMap<>();
+    }
+    
+    public void push(int val) {
+        map.put(val,map.getOrDefault(val,0)+1);
+        int v = map.get(val);
+        
+        if(v-1>=list.size()){
+            list.add(new Stack<Integer>());
+        }
+        
+        list.get(v-1).add(val);
+    }
+    
+    public int pop() {
+        int freq = list.size();
+        int x = list.get(freq - 1).pop();
+        if (list.get(freq - 1).isEmpty()) list.remove(list.size() - 1);
+        
+        map.put(x, map.get(x) - 1);
+        if (map.get(x) == 0) map.remove(x);
+        
+        return x;
+        
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
